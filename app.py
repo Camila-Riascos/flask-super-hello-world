bdf6b2e914ad4e09b29361569902b07025472b04
Cammila-Riascos <camilariascos573@gmail.com>
2023-06-24 16:33:31 -0400

    tarea lista

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
+
 @app.route('/welcome')
 def welcome():
-    return render_template ('welcome.html', user = request.orgs.get('username') )
+    return render_template('welcome.html', user=request.args.get('username'))
+
+
+appy.py
